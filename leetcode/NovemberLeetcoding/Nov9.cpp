82d17d3998d0b547746dc5d008d44a3c08f67100
260200 <mangal.nitya2602@gmail.com>
2020-11-09 17:23:02 +0530

    max diff b/w node & ancestor - 3 approaches

--- leetcode/NovemberLeetcoding/Nov9.cpp
+++ leetcode/NovemberLeetcoding/Nov9.cpp
@@ -9,12 +9,12 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+typedef pair<int, int> PAIR;
 class Solution {
 public:
     
     //we calculate difference for each node and save the max difference
     //for each node we have both max and min ancestor
-    //finding max and min descendant for each node would require repeated traversals
     void preorder(TreeNode* root, int currMin, int currMax, int *difference)
     {
         if(root == NULL)
@@ -26,6 +26,21 @@ public:
         preorder(root -> right, min(currMin, root -> val), max(currMax, root -> val), difference);
     }
     
+    //to find max and min value from subtree(descendants) instead of ancestors
+    PAIR trial(TreeNode* root, int currMin, int currMax, int *difference)
+    {
+        if(root == NULL)
+            return {INT_MAX, INT_MIN};
+        if(root->left == NULL && root->right == NULL)
+            return {root->val, root->val};
+        PAIR leftSubtree, rightSubtree;
+        leftSubtree = trial(root -> left, currMin, currMax, difference);
+        rightSubtree = trial(root -> right, currMin, currMax, difference);
+        int MIN = min(leftSubtree.first, rightSubtree.first);
+        int MAX = max(leftSubtree.second, rightSubtree.second);
+        *difference = max(*difference, max(abs(root->val-MIN), abs(root->val-MAX)));
+        return {min(MIN, root->val), max(MAX, root->val)};
+    }
     //for each root to leaf path, we maintain max an min value and take 
     //their difference on reaching the leaf node and propogate it back upwards
     int postorder(TreeNode* root, int currMin, int currMax)
@@ -38,8 +53,10 @@ public:
     }
     int maxAncestorDiff(TreeNode* root) {
         int difference = 0;
+        if(root == NULL) return 0;
         //preorder(root, root -> val, root -> val, &difference);
-        difference = postorder(root, root -> val, root -> val);
+        //difference = postorder(root, root -> val, root -> val);
+        trial(root, INT_MAX, INT_MIN, &difference);
         return difference;
     }
 };
